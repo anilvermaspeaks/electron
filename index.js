bfadb932f1a47492fd3e057a04395e7bfe1af74c
anil verma <anilcs240@gmail.com>
2021-08-09 18:54:19 +0530

    created menu bar and new window

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const { app, BrowserWindow, ipcMain } = require('electron')
+const { app, BrowserWindow, ipcMain, Menu } = require('electron')
 const path = require('path')
 const ffmpeg = require('fluent-ffmpeg');
-
+const isMac = process.platform === 'darwin'
 let win;
+let addWindow;
 function createWindow() {
     win = new BrowserWindow({
         width: 1000,
@@ -11,11 +12,16 @@ function createWindow() {
             preload: path.join(app.getAppPath(), 'preload.js')
         },
     });
-    win.loadFile('index.html')
+    win.loadFile('index.html');
+    const mainMenu = Menu.buildFromTemplate(menuTemplate)
+    Menu.setApplicationMenu(mainMenu)
 }
 
 app.whenReady().then(() => {
     createWindow()
+    win.on('closed', () => {
+        app.quit()
+    })
 })
 
 ipcMain.on('video:submit', (event, videoPath) => {
@@ -27,5 +33,58 @@ ipcMain.on('video:submit', (event, videoPath) => {
 
 
 app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') app.quit()
+    if (!isMac) app.quit()
 })
+
+function openNewWindow() {
+    addWindow = new BrowserWindow({
+        width: 400,
+        height: 200,
+        title: 'Say Hello'
+    })
+    addWindow.loadFile('hello.html');
+    addWindow.on('closed', () => {
+        addWindow = null
+    })
+}
+
+const menuTemplate = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Say Hello',
+                accelerator: isMac ? 'Command+H' : 'Ctrl+H',
+                click() {
+                    openNewWindow()
+                }
+            },
+            {
+                label: 'Quit',
+                accelerator: isMac ? 'Command+Q' : 'Ctrl+Q',
+                click() {
+                    app.quit()
+                }
+            }
+        ]
+    }
+]
+
+if (isMac) {
+    menuTemplate.unshift({})
+}
+
+if (process.env.NODE_ENV !== 'production') {
+    menuTemplate.push({
+        label: 'Dev Tool',
+        submenu: [{
+            label: 'Toggle Developer Tool',
+            accelerator: isMac ? 'Command+I' : 'Ctrl+I',
+            click(item, focusedWindow) {
+                focusedWindow.toggleDevTools();
+
+            }
+        }]
+    })
+
+}
